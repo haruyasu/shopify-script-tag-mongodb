e45d7a5be3a2e440fcd31c2b78d56291903377c9
haruyasu <harukun2002@gmail.com>
2021-05-16 13:46:07 +0900

    finish

--- public/test-script.js
+++ public/test-script.js
@@ -1,16 +1,25 @@
 // const APP_URl = 'https://script-tags.herokuapp.com'
 const APP_URl = 'https://8d1e817c852b.ngrok.io'
-const body = $('body')
 
-body.css({
-  position: 'relative',
-})
+const script = document.createElement('script')
+script.src = 'https://code.jquery.com/jquery-3.4.1.min.js'
+script.type = 'text/javascript'
+script.onreadystatechange = handler
+script.onload = handler
+document.getElementsByTagName('head')[0].appendChild(script)
 
-const shop = Shopify.shop
+function handler() {
+  const body = $('body')
 
-const makeApp = (products) => {
-  const bestSellerContainer = $(
-    `<div style="overflow-y: scroll;">
+  body.css({
+    position: 'relative',
+  })
+
+  const shop = Shopify.shop
+
+  const makeApp = (products) => {
+    const bestSellerContainer = $(
+      `<div style="overflow-y: scroll;">
             <h3>売上トップ</h3>
             ${products
               .map((item) => {
@@ -26,43 +35,45 @@ const makeApp = (products) => {
               })
               .join('')}
         </div>`
-  ).css({
-    position: 'fixed',
-    'background-color': '#ffffff',
-    padding: '10px',
-    border: '1px solid black',
-    bottom: '80px',
-    right: '25px',
-    height: '400px',
-    width: '350px',
-    display: 'none',
-  })
-
-  const bestSellerButton = $('<img />')
-    .attr('src', `${APP_URl}/top.png`)
-    .css({
+    ).css({
       position: 'fixed',
-      width: '150px',
-      bottom: '20px',
-      right: '20px',
-      cursor: 'pointer',
+      'background-color': '#ffffff',
+      padding: '10px',
+      border: '1px solid black',
+      bottom: '80px',
+      right: '25px',
+      height: '400px',
+      width: '400px',
+      display: 'none',
+      'z-index': 99
     })
 
-  body.append(bestSellerButton)
-  body.append(bestSellerContainer)
+    const bestSellerButton = $('<img />')
+      .attr('src', `${APP_URl}/top.png`)
+      .css({
+        position: 'fixed',
+        width: '150px',
+        bottom: '20px',
+        right: '20px',
+        cursor: 'pointer',
+      })
 
-  bestSellerButton.click(() => {
-    bestSellerContainer.slideToggle()
-  })
+    body.append(bestSellerButton)
+    body.append(bestSellerContainer)
+
+    bestSellerButton.click(() => {
+      bestSellerContainer.slideToggle()
+    })
+  }
+
+  // fetch(APP_URl + '/api/products?shop=' + shop)
+  fetch(
+    `https://cors-anywhere.herokuapp.com/${APP_URl}/api/products?shop=${shop}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      makeApp(data.data)
+      console.log(data)
+    })
+    .catch((error) => console.log(error))
 }
-
-// fetch(APP_URl + '/api/products?shop=' + shop)
-fetch(
-  `https://cors-anywhere.herokuapp.com/${APP_URl}/api/products?shop=${shop}`
-)
-  .then((res) => res.json())
-  .then((data) => {
-    makeApp(data.data)
-    console.log(data)
-  })
-  .catch((error) => console.log(error))

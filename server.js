9c677a54a3ea640af8c1cb07cd65a165f0ee1009
haruyasu <harukun2002@gmail.com>
2021-05-16 13:23:56 +0900

    add mongodb

--- server.js
+++ server.js
@@ -74,61 +74,61 @@ router.get('/api/fetchProduct', async (ctx) => {
 
 // Get Shop products for store front
 router.get('/api/products', koaBody(), async (ctx) => {
-  try {
-    ctx.body = {
-      status: 'success',
-      data: products,
-    }
-  } catch (error) {
-    console.log(error)
-  }
-
-  // const query = ctx.request.query
-  // if (query.shop) {
-  //   const products = await product.find({ domain: query.shop }).toArray()
+  // try {
   //   ctx.body = {
   //     status: 'success',
   //     data: products,
   //   }
-  // } else {
-  //   ctx.body = {
-  //     status: 500,
-  //     message: 'Shop is required',
-  //   }
+  // } catch (error) {
+  //   console.log(error)
   // }
+
+  const query = ctx.request.query
+  if (query.shop) {
+    const products = await product.find({ domain: query.shop }).toArray()
+    ctx.body = {
+      status: 'success',
+      data: products,
+    }
+  } else {
+    ctx.body = {
+      status: 500,
+      message: 'Shop is required',
+    }
+  }
 })
 
 // Save shop products in database
 router.post('/api/products', koaBody(), async (ctx) => {
   try {
     const body = ctx.request.body
-    // const { shop, accessToken } = ctx.session
+    const { shop, accessToken } = ctx.session
 
-    // // Check product exists in db
-    // await product.findOne(
-    //   { domain: shop, id: body.id },
-    //   async (error, result) => {
-    //     if (error) {
-    //       ctx.body = {
-    //         status: 500,
-    //         error: error,
-    //       }
-    //     }
-    //     if (!result) {
-    //       // Insert data in product table
-    //       body.domain = shop
-    //       await product.insertOne(body, (error, result) => {
-    //         if (error) {
-    //           console.log('error1', error)
-    //         } else {
-    //           console.log('Product added in db')
-    //         }
-    //       })
-    //     } else {
-    //       console.log('Product already exists in db')
-    //     }
-    //   }
-    // )
+    // Check product exists in db
+    await product.findOne(
+      { domain: shop, id: body.id },
+      async (error, result) => {
+        if (error) {
+          ctx.body = {
+            status: 500,
+            error: error,
+          }
+        }
+        if (!result) {
+          // Insert data in product table
+          body.domain = shop
+          await product.insertOne(body, (error, result) => {
+            if (error) {
+              console.log('error1', error)
+            } else {
+              console.log('Product added in db')
+            }
+          })
+        } else {
+          console.log('Product already exists in db')
+        }
+      }
+    )
 
     await products.push(body)
     ctx.body = 'Item Added'
@@ -139,37 +139,37 @@ router.post('/api/products', koaBody(), async (ctx) => {
 
 // Delete products of shop from databsase
 router.delete('/api/products', koaBody(), async (ctx) => {
-  try {
-    products = []
-    ctx.body = 'All items deleted!'
-  } catch (error) {
-    console.log(error)
-  }
-  // const { shop, accessToken } = ctx.session
   // try {
   //   products = []
-  //   await product.deleteMany({ domain: shop })
   //   ctx.body = 'All items deleted!'
   // } catch (error) {
   //   console.log(error)
   // }
+  const { shop, accessToken } = ctx.session
+  try {
+    products = []
+    await product.deleteMany({ domain: shop })
+    ctx.body = 'All items deleted!'
+  } catch (error) {
+    console.log(error)
+  }
 })
 
 // Receive weebbhook
-// const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY })
+const webhook = receiveWebhook({ secret: SHOPIFY_API_SECRET_KEY })
 
 // App unistall weebhook
-// router.post('/uninstall', webhook, (ctx) => {
-//   // console.log('received webhook: ', ctx.state.webhook);
-//   // console.log('received webhook: ', ctx.request.body);
-//   const shop = ctx.request.body.domain
-//   if (shop) {
-//     store.deleteOne({ domain: shop })
-//     product.deleteMany({ domain: shop })
-//     console.log(shop)
-//     console.log('shop data deleted')
-//   }
-// })
+router.post('/uninstall', webhook, (ctx) => {
+  // console.log('received webhook: ', ctx.state.webhook);
+  // console.log('received webhook: ', ctx.request.body);
+  const shop = ctx.request.body.domain
+  if (shop) {
+    store.deleteOne({ domain: shop })
+    product.deleteMany({ domain: shop })
+    console.log(shop)
+    console.log('shop data deleted')
+  }
+})
 
 // Database connection
 const uri =
@@ -179,7 +179,9 @@ const uri =
   DB_PASSWORD +
   '@' +
   DB_HOST +
-  '/test?retryWrites=true&w=majority'
+  '/' +
+  DB_NAME +
+  '?retryWrites=true&w=majority'
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -206,69 +208,69 @@ app.prepare().then(() => {
       async afterAuth(ctx) {
         const { shop, accessToken } = ctx.session
         console.log(shop)
-        // // Check store exists in db
-        // await store.findOne({ domain: shop }, async (error, result) => {
-        //   if (error) {
-        //     ctx.body = {
-        //       status: 500,
-        //       error: error,
-        //     }
-        //   }
-        //   if (!result) {
-        //     // Register a webhook for app uninstall
-        //     const registration = await registerWebhook({
-        //       // for local dev you probably want ngrok or something similar
-        //       address: `${APP_URl}/uninstall`,
-        //       topic: 'APP_UNINSTALLED',
-        //       format: 'json',
-        //       accessToken,
-        //       shop,
-        //       apiVersion: ApiVersion.Unstable,
-        //     })
+        // Check store exists in db
+        await store.findOne({ domain: shop }, async (error, result) => {
+          if (error) {
+            ctx.body = {
+              status: 500,
+              error: error,
+            }
+          }
+          if (!result) {
+            // Register a webhook for app uninstall
+            const registration = await registerWebhook({
+              // for local dev you probably want ngrok or something similar
+              address: `${APP_URl}/uninstall`,
+              topic: 'APP_UNINSTALLED',
+              format: 'json',
+              accessToken,
+              shop,
+              apiVersion: ApiVersion.Unstable,
+            })
 
-        //     if (registration.success) {
-        //       console.log('Successfully registered webhook!')
+            if (registration.success) {
+              console.log('Successfully registered webhook!')
 
-        //       // Register script tag
-        //       const scriptTagJsonUrl = `https://${shop}/admin/api/2019-10/script_tags.json`
-        //       const response = await fetch(scriptTagJsonUrl, {
-        //         method: 'post',
-        //         headers: {
-        //           'X-Shopify-Access-Token': accessToken,
-        //           Accept: 'application/json',
-        //           'Content-Type': 'application/json',
-        //         },
-        //         body: JSON.stringify({
-        //           script_tag: {
-        //             event: 'onload',
-        //             src: `${APP_URl}/custom.js`,
-        //           },
-        //         }),
-        //       })
+              // Register script tag
+              const scriptTagJsonUrl = `https://${shop}/admin/api/2019-10/script_tags.json`
+              const response = await fetch(scriptTagJsonUrl, {
+                method: 'post',
+                headers: {
+                  'X-Shopify-Access-Token': accessToken,
+                  Accept: 'application/json',
+                  'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                  script_tag: {
+                    event: 'onload',
+                    src: `${APP_URl}/custom.js`,
+                  },
+                }),
+              })
 
-        //       // const responseBody = await response.json();
-        //       // console.log(responseBody, `=====responseBody in create st=====`);
+              // const responseBody = await response.json();
+              // console.log(responseBody, `=====responseBody in create st=====`);
 
-        //       // Insert data in store table
-        //       const storeresponse = {
-        //         domain: shop,
-        //         accessToken: accessToken,
-        //       }
-        //       await store.insertOne(storeresponse, (error, result) => {
-        //         if (error) {
-        //           console.log('error1', error)
-        //         } else {
-        //           console.log('store detail inserted into db')
-        //         }
-        //       })
-        //     } else {
-        //       console.log('Failed to register webhook', registration.result)
-        //     }
-        //   } else {
-        //     console.log('store detail already exsit in db')
-        //   }
-        // })
-        
+              // Insert data in store table
+              const storeresponse = {
+                domain: shop,
+                accessToken: accessToken,
+              }
+              await store.insertOne(storeresponse, (error, result) => {
+                if (error) {
+                  console.log('error1', error)
+                } else {
+                  console.log('store detail inserted into db')
+                }
+              })
+            } else {
+              console.log('Failed to register webhook', registration.result)
+            }
+          } else {
+            console.log('store detail already exsit in db')
+          }
+        })
+
         ctx.cookies.set('shopOrigin', shop, {
           httpOnly: false,
           secure: true,
@@ -289,20 +291,22 @@ app.prepare().then(() => {
     return
   })
 
-  server.listen(port, () => {
-    console.log(`> Ready on http://localhost:${port}`)
-  })
-
+  // ローカル
   // server.listen(port, () => {
-  //   client.connect((err) => {
-  //     if (err) {
-  //       console.log('Error in database connection ' + err)
-  //       throw err
-  //     }
-  //     global.store = client.db(DB_NAME).collection('stores')
-  //     global.product = client.db(DB_NAME).collection('products')
-  //     console.log('Connected with db')
-  //     console.log(`> Ready on http://localhost:${port}`)
-  //   })
+  //   console.log(`> Ready on http://localhost:${port}`)
   // })
+
+  // MongoDB
+  server.listen(port, () => {
+    client.connect((err) => {
+      if (err) {
+        console.log('Error in database connection ' + err)
+        throw err
+      }
+      global.store = client.db(DB_NAME).collection('stores')
+      global.product = client.db(DB_NAME).collection('products')
+      console.log('Connected with db')
+      console.log(`> Ready on http://localhost:${port}`)
+    })
+  })
 })
